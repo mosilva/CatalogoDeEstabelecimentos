f6bcb3d6503ba2d8193eb6650df63d340c405e9a
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-11-14 12:20:17 -0300

    Create main table

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+let CatalogEstablishment = [
+    {
+        "Categoria": "Doce",
+        "Nome": "Cacau Show",
+        "Endereco": "Giovanni Gronchi, 5819 - Piso 1 - Loja 01",
+        "Cep": "05724-003",
+        "Telefone": 1158457187,
+        "Email": "dengoChocolateCacauShow@gmail.com"
+    },
+    {
+        "Categoria": "Fast Food",
+        "Nome": "Burguer King",
+        "Endereco": "Estrada do Campo Limpo, 459",
+        "Cep": "05777-001",
+        "Telefone": 1158401212,
+        "Email": "bkCampoLimpo@hotmail.com"
+    },
+    ]
+
+
 function generateHeader(){
     const header__Catalogo = document.createElement("header");
     const header__logo = document.createElement("div");
@@ -6,6 +26,9 @@ function generateHeader(){
     header__Catalogo.appendChild(header__logo);
 
 }
+
+const header = ["Categoria", "Nome", "Endereço", "CEP","Telefone","E-mail"];
+
 
 function generateMain(){
     const main__Catalogo = document.createElement("main");
@@ -17,13 +40,47 @@ function generateMain(){
     const h2__Catalogo = document.createElement("h2");
     h2__Catalogo.textContent = "Catálogo de Estabelecimentos";
     section__Catalogo.appendChild(h2__Catalogo);
-};
 
-function generateCatalogOfEstablishments(){
     const table__main__catalogo = document.createElement("table");
+    section__Catalogo.appendChild(table__main__catalogo);
+    const table__main__thead = document.createElement("thead");
+    table__main__catalogo.appendChild(table__main__thead);
 
+    const table__main__theadTr = document.createElement("tr");
+    table__main__theadTr.setAttribute("class","catalog-table");
+    table__main__thead.appendChild(table__main__theadTr);
+
+    const table__main__tbody = document.createElement("tbody");
+    table__main__tbody.setAttribute("class","establishment-table");
+
+
+
+    for (const element of header) {
+        const table__main__theadTh = document.createElement("th");
+        const texti = document.createTextNode(element);
+        table__main__theadTh.appendChild(texti);
+        table__main__theadTr.appendChild(table__main__theadTh);
+        table__main__thead.appendChild(table__main__theadTr);
+    }
+
+    for (const element of CatalogEstablishment) {
+        let establishment = element;
+        const table__main__tbodyTr = document.createElement("tr");
+        table__main__tbodyTr.setAttribute("class","establishment");
+        table__main__tbodyTr.setAttribute("class", "body__information");
+
+        for (let k = 0; k < header.length; k++) {
+          const table__main__tbodyTd = document.createElement("td");
+          table__main__tbodyTd.textContent = Object.values(establishment)[k];
+          table__main__tbodyTd.setAttribute("class","info-"+ Object.keys(establishment)[k]);
+          table__main__tbodyTr.appendChild(table__main__tbodyTd);
+
+          table__main__tbody.appendChild(table__main__tbodyTr);
+          table__main__catalogo.appendChild(table__main__tbody);
+
+        }
+      }   
 };
-
 
 
 
