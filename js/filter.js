a20319294ecf15427bbd41943c9643249ccbf90f
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-11-16 10:48:32 -0300

    Update filter

--- js/filter.js
+++ js/filter.js
@@ -4,16 +4,26 @@ filterField.addEventListener("input", function(){
 
     let establishments = document.querySelectorAll(".body__information");
     
-    for(const element of establishments)
+    if(this.value.length > 0)
     {
-        const tdName = element.querySelector(".info-Nome");
-        const name = tdName.textContent;
-        const inputName = this.value.toLowerCase()
-        if(name.toLowerCase() != this.value.toLowerCase()){
-            invisibleEstab(element);
-        }else{
-            visibleEstab(element);
+        for(const element of establishments)
+        {
+            const tdName = element.querySelector(".info-Nome");
+            const name = tdName.textContent;
+            const inputName = this.value.toLowerCase()
+            if(name.toLowerCase() != this.value.toLowerCase()){
+                invisibleEstab(element);
+            }
+            else
+            {
+                visibleEstab(element);
+            }
         }
     }
-    
+    else
+    {
+        for(const element of establishments){
+                visibleEstab(element);    
+        } 
+    }
 });
