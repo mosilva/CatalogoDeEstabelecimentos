ea58f9a276ff83220af003759bbe25b1aa609862
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-11-25 07:18:36 -0300

    update home

--- js/home.js
+++ js/home.js
@@ -1,68 +1,79 @@
 (() => {
-    const homeStyle = document.createElement("script");
-    homeStyle.setAttribute("src", "js/styles/homeStyle.js");
-    document.body.appendChild(homeStyle);
-  })();
+  const homeStyle = document.createElement("script");
+  homeStyle.setAttribute("src", "js/styles/homeStyle.js");
+  document.body.appendChild(homeStyle);
+})();
 
-function carouselGenerate(){
+for (const files of [
+  "./js/common/functions.js",
+  "./js/styles/general-styles.js",
+  "./js/style.js",
+  "./js/apis.js",
+  "./js/header.js",
+  "./js/establishments.js",
+  "./js/category.js",
+  "./js/footer.js",
+]) {
+  const script = document.createElement("script");
+  script.setAttribute("src", `${files}`);
+  document.head.appendChild(script);
+}
 
-    const home = document.createElement("section");
-    home.classList.add('homeContainer');
+function carouselGenerate() {
+  const home = document.createElement("section");
+  home.classList.add("homeContainer");
 
-    const main = document.querySelector("main");
-    if(main){
-        main.appendChild(home);
-    }
-    else{
-        const main = document.createElement('main');
-        document.body.appendChild(main);
-        main.appendChild(home);
-    }
-   
+  const main = document.querySelector("main");
+  if (main) {
+    main.appendChild(home);
+  } else {
+    const main = document.createElement("main");
+    document.body.appendChild(main);
+    main.appendChild(home);
+  }
 
+  const title = document.createElement("h1");
+  title.textContent = "Últimos estabelecimentos adicionados";
+  home.appendChild(title);
+  const firstDiv = document.createElement("div");
+  home.appendChild(firstDiv);
 
-    const title = document.createElement("h1");
-    title.textContent = "Últimos estabelecimentos adicionados";
-    home.appendChild(title);
-    const firstDiv = document.createElement("div");
-    home.appendChild(firstDiv);
+  const firstImg = document.createElement("img");
+  firstDiv.appendChild(firstImg);
+  const secondImg = document.createElement("img");
+  firstDiv.appendChild(secondImg);
+  const thirdImg = document.createElement("img");
+  firstDiv.appendChild(thirdImg);
 
-    const firstImg = document.createElement("img");
-    firstDiv.appendChild(firstImg);
-    const secondImg = document.createElement("img");
-    firstDiv.appendChild(secondImg);
-    const thirdImg = document.createElement("img");
-    firstDiv.appendChild(thirdImg);
-
-    function setAttributesHome(){
-        title.setAttribute("class", "title");
-        firstImg.setAttribute("src", "../img/mcDonalds.jpg");
-        firstImg.setAttribute("class", "carouselImg");
-         firstDiv.setAttribute("class", "container");
-         secondImg.setAttribute("src", "../img/palacioDeTangara.jpg");
-         secondImg.setAttribute("class", "carouselImg");
-         thirdImg.setAttribute("src", "../img/dalvaEDito.jpg");
-        thirdImg.setAttribute("class", "carouselImg");
-    }
-    setAttributesHome();
+  function setAttributesHome() {
+    title.setAttribute("class", "title");
+    firstImg.setAttribute("src", "../img/mcDonalds.jpg");
+    firstImg.setAttribute("class", "carouselImg");
+    firstDiv.setAttribute("class", "container");
+    secondImg.setAttribute("src", "../img/palacioDeTangara.jpg");
+    secondImg.setAttribute("class", "carouselImg");
+    thirdImg.setAttribute("src", "../img/dalvaEDito.jpg");
+    thirdImg.setAttribute("class", "carouselImg");
+  }
+  setAttributesHome();
 }
 
 carouselGenerate();
 
 let interval = 0;
-let maxImgs = document.querySelectorAll(".carouselImg").length -1;
+let maxImgs = document.querySelectorAll(".carouselImg").length - 1;
 
-function carouselAction(){
-    let img = document.querySelectorAll(".carouselImg");
+function carouselAction() {
+  let img = document.querySelectorAll(".carouselImg");
 
-    setInterval(function(){
-        img[interval].style.display = 'none';
-        interval++;
-        if(interval > maxImgs){
-            interval = 0;
-        }
-        img[interval].style.display = 'block';
-    }, 1500)
+  setInterval(function () {
+    img[interval].style.display = "none";
+    interval++;
+    if (interval > maxImgs) {
+      interval = 0;
+    }
+    img[interval].style.display = "block";
+  }, 1500);
 }
 
 carouselAction();
